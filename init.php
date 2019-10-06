a2a0b7e5ec2a0b1320b368ea8e98eeaa1c7179dd
haog1 <gaohaoqian@gmail.com>
2019-10-06 23:32:08 +1100

    setup panel

--- init.php
+++ init.php
@@ -1,10 +1,23 @@
 <?php
 
+add_action( 'admin_menu', 'add_admin_menu' );
+
+/**
+ * Adds a the new item to the admin menu.
+ */
+function add_admin_menu() {
+  add_submenu_page( 'tools.php', 'QR Code Generator', 'QR Code Generator', 'manage_options', 'tools.php?page=qr-code-generator', 'render_admin_panel' );
+}
+
+function render_admin_panel() {
+  echo '<div class="wrapper">';
+  echo '<h1>QR Code Generator</h1>';
+  echo '</div>';
+}
+
 function generateQRCode( $data ) {
   if( !$data ) {
     return;
   }
-
-  QRcode::png( $data );
-
+  // QRcode::png( $data );
 }
--- qrcode.php
+++ qrcode.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: QR Code Generator
- * Plugin URI: https://glorycitychurch.com
+ * Plugin Name: GCC QR Code Generator
+ * Plugin URI: https://github.com/haog1/gcc-qrcode
  * Description: The QR code generator for Glory City Church of Melbourne, Inspired by PPHPQRCode Library
  * Version: 1.0
  * Author: Tony Gao
