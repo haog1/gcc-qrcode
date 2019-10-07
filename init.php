e439b20220d99e6630fa3886269c14182745bc2a
haog1 <gaohaoqian@gmail.com>
2019-10-08 00:21:24 +1100

    created generate qr code functions

--- init.php
+++ init.php
@@ -1,15 +1,18 @@
 <?php
 
-add_action( 'admin_menu', 'add_admin_menu' );
+add_action( 'admin_menu', 'addAdminMenu' );
+add_action( 'add_meta_boxes', 'addQRCodeMetaBox' );
+add_action("wp_ajax_gcc_qr_code_generate", 'handleGenerateQRCode' );
+add_action("wp_ajax_nopriv_gcc_qr_code_generate", 'handleGenerateQRCode' );
 
 /**
  * Adds a the new item to the admin menu.
  */
-function add_admin_menu() {
-  add_submenu_page( 'tools.php', 'QR Code Generator', 'QR Code Generator', 'manage_options', 'qr-code-generator', 'render_admin_panel' );
+function addAdminMenu() {
+  add_submenu_page( 'tools.php', 'QR Code Generator', 'QR Code Generator', 'manage_options', 'qr-code-generator', 'renderAdminPanel' );
 }
 
-function render_admin_panel() {
+function renderAdminPanel() {
   ?>
   <div class="gcc-qr-code--wrapper">
     <h1 class="gcc-qr-code--title">Generate QR Code</h1>
@@ -49,9 +52,58 @@ function render_admin_panel() {
   <?php
 }
 
-function generateQRCode( $data ) {
+function addQRCodeMetaBox() {
+  $screens = ['page', 'book'];
+  foreach ($screens as $screen) {
+    add_meta_box( 'gcc-qr-code-for-' . $screen, 'Generate QR Code', 'generateQRCodeHTML', $screen, 'side', 'low' );
+  }
+}
+
+function generateQRCodeHTML() {
+  global $post;
+  ?>
+  <div class="gcc-qr-code--meta-box-wrapper" data-name="<?= $post->post_type ?>" data-id="<?= $post->ID ?>">
+    <a href="javascript:void(0);" class="button button-primary button-large generate-qr-code-link">
+      Generate
+    </a>
+  </div>
+  <?php
+}
+
+function handleGenerateQRCode() {
+  $post_type = array_get( $_POST, 'name' );
+  $post_id = array_get( $_POST, 'id' );
+  if(!$post_type) {
+    exit;
+  }
+  if(!$post_id) {
+    exit;
+  }
+  $string = '/resources/library?post_type=' . $post_type . '?post_id=' . $post_id;
+  $data = get_home_url() . $string;
+  $path = generateQRCode($data, $post_id);
+  echo json_encode([
+    'url' => wp_upload_dir()['baseurl'] . $path,
+    'status' => 200 ]);
+  exit;
+}
+
+function generateQRCode( $data, $id ) {
   if( !$data ) {
     return;
   }
-  // QRcode::png( $data );
+  $base = wp_upload_dir();
+  $path = $base['basedir'] . '/qr-code-pngs';
+
+  if (!file_exists($path)) {
+    mkdir($path, 0777, true);
+  }
+
+  $filename = $path. '/qr-code-' . $id . '.png';
+
+  if (!file_exists($filename)) {
+    QRcode::png($data , $filename);
+  }
+
+  return '/qr-code-pngs/qr-code-' . $id . '.png';
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $( document ).ready( function() {
-  console.log(1);
+
+  $( 'a.generate-qr-code-link' ).on( 'click', function ( e ) {
+    e.preventDefault();
+
+    let data = {
+      name: $( '.gcc-qr-code--meta-box-wrapper' ).data( 'name' ),
+      id: $( '.gcc-qr-code--meta-box-wrapper' ).data( 'id' ),
+      action: 'gcc_qr_code_generate',
+    }
+
+    $.ajax({
+      url: window.ajaxurl,
+      method: 'POST',
+      dataType: 'json',
+      data: data,
+  }).done(function(response) {
+    let src = '<img class="standalone-png" src="' + response.url + '">';
+    $('.gcc-qr-code--meta-box-wrapper').append(src);
+  });
+
+  });
+
 });
