89f595faefd54b8c8016d48da4b5d24d56dd99e4
haog1 <gaohaoqian@gmail.com>
2019-10-13 22:42:49 +1100

    update error

--- main.php
+++ main.php
@@ -1,59 +1,8 @@
 <?php
 
-if(array_get($_GET, 'action') != 'edit') {
-  return;
-}
-
-add_action( 'admin_menu', 'addAdminMenu' );
 add_action( 'add_meta_boxes', 'addQRCodeMetaBox' );
 add_action("wp_ajax_gcc_qr_code_generate", 'handleGenerateQRCode' );
 add_action("wp_ajax_nopriv_gcc_qr_code_generate", 'handleGenerateQRCode' );
-
-/**
- * Adds a the new item to the admin menu.
- */
-function addAdminMenu() {
-  add_submenu_page( 'tools.php', 'QR Code Generator', 'QR Code Generator', 'manage_options', 'qr-code-generator', 'renderAdminPanel' );
-}
-
-function renderAdminPanel() {
-  ?>
-  <div class="gcc-qr-code--wrapper">
-    <h1 class="gcc-qr-code--title">Generate QR Code</h1>
-    <h2 class="gcc-qr-code--sub-title">Choose a Post Type</h2>
-    <form class="gcc-qr-code--form" action="tools.php?page=qr-code-generator?method=generate_qrcode" method="post">
-      <select id="gcc-qr-code--selector" required="true" multiple style="min-height: 120px;min-width: 150px">
-        <?php
-          foreach(get_post_types() as $value => $name ) {
-            if( $value != 'nav_menu_item' &&
-                $value != 'acf-field' &&
-                $value != 'acf-field-group' &&
-                $value != 'schema' &&
-                $value != 'wp_block' &&
-                $value != 'user_request' &&
-                $value != 'customize_changeset' &&
-                $value != 'custom_css' &&
-                $value != 'oembed_cache' &&
-                $value != 'revision' ) {
-              echo '<option value="' . $value . '">' . $name . '</option>';
-            }
-          }
-        ?>
-      </select>
-      <div class="gcc-qr-code--clear clear" style="margin-bottom: 30px;"></div>
-      <button class="gcc-qr-code--btn button button-primary button-hero">Generate</button>
-    </form>
-    <div class="gcc-qr-code--container">
-        <div class="gcc-qr-code--list-wrapper">
-          <h2 class="gcc-qr-code--sub-title">Results are shown below:</h2>
-          <div class="gcc-qr-code--max-height-container" style="max-height: 450px;overflow: scroll">
-            <div class="gcc-qr-code--inner-wrapper"></div>
-          </div>
-        </div>
-    </div>
-  </div>
-  <?php
-}
 
 function addQRCodeMetaBox() {
   foreach (get_post_types() as $screen) {
@@ -67,7 +16,9 @@ function addQRCodeMetaBox() {
     $value != 'custom_css' &&
     $value != 'oembed_cache' &&
     $value != 'revision' ) {
-      add_meta_box( 'gcc-qr-code-for-' . $screen, 'Generate QR Code', 'generateQRCodeHTML', $screen, 'side', 'low' );
+      if(isset($_GET['action']) && $_GET['action'] == 'edit') {
+        add_meta_box( 'gcc-qr-code-for-' . $screen, 'Generate QR Code', 'generateQRCodeHTML', $screen, 'side', 'low' );
+      }
     }
   }
 }
